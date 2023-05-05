747d04e410c84f91df8c934fde7ab48ac03b3cf9
wjz <wjz@163.com>
2023-05-05 18:10:54 +0800

    wjz first commit!

---
+++ src/main/java/com/zpark/ssm/App.java
@@ -0,0 +1,13 @@
+package com.zpark.ssm;
+
+/**
+ * @auther wujunzhou
+ * @create 2023/5/5 17:37
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}

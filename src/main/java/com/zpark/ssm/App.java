a8840e2b03b63072e6334c22f485297f3e5af3c6
wjz <wjz@163.com>
2023-05-06 09:05:46 +0800

    wjz hotfix first commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -9,6 +9,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! wjz master second commit!");
+        System.out.println("Hello Git! wjz hotfix first commit!");
     }
 
 }
